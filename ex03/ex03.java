e701d8051dc095d2c276ff06361f3497a1a0d4c1
snielsen221b <sophianielsen221b@gmail.com>
2016-11-13 19:48:51 -0500

    ADD: some dog pack stuff

--- ex03/ex03.java
+++ ex03/ex03.java
@@ -1,9 +1,17 @@
 //def of dog class
 class Dog {
 
-  private int size;
+  int size;
   String breed;
   String name;
+
+  public Dog() {}
+
+  public Dog(int size, String breed, String name) {
+    this.size = size;
+    this.breed = breed;
+    this.name = name;
+  }
 
   public int getSize() {
     return size;
@@ -40,6 +48,60 @@ class Cat {
   void bark() {
     System.out.println("I'm a cat");
   }
+}
+
+class DogPack {
+
+  private Dog[] dogs;
+
+  void makeArray(int size) {
+    dogs = new Dog[size];
+  }
+
+  void addDog(int index, Dog aDog) {
+    dogs[index] = aDog;
+  }
+
+  Dog biggestDog() {
+    Dog biggestSize = dogs[0];
+    for (Dog dog:dogs) {
+      if (dog.isBigger(biggestSize)) {
+        biggestSize = dog;
+      }
+    }
+    return biggestSize;
+  }
+
+  void showDogs() {
+    System.out.println("Dogs in this pack:");
+    for (Dog d : dogs) {
+      if (d != null) {
+        System.out.println("(" + d.size + ", " + d.breed + ", " + d.name + ")");
+      } else {
+        System.out.println("No dog here");
+      }
+    }
+  }
+
+  public static void main (String[] args) {
+    // int[] sizes = new int[]{3,4,5,6};
+    //
+    // for (int size:sizes) {
+    //   DogPack aDogPack = new DogPack();
+    //   aDogPack.makeArray(size);
+    //   aDogPack.addDog(0, new Dog(20, "Beagle", "Lennie"));
+    //   aDogPack.showDogs();
+    // }
+    DogPack aDogPack = new DogPack();
+    aDogPack.makeArray(3);
+    aDogPack.addDog(0, new Dog(4, "Beagle", "Fred"));
+    aDogPack.addDog(1, new Dog(23, "Beagle", "George"));
+    aDogPack.addDog(2, new Dog(5, "Beagle", "Ron"));
+    aDogPack.showDogs();
+
+    System.out.println(aDogPack.biggestDog().name);
+  }
+
 }
 
 class DogTestDrive {
